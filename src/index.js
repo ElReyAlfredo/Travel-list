cd788c2c4356b3cbd1f73d8c7ed3342e3d0081a0
ElReyAlfredo <jdelgado7@ucol.mx>
2024-01-14 22:08:25 -0600

    Final Version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-//Import React
 import React from "react";
-//Import ReactDOM
 import ReactDOM from "react-dom/client";
-//Import index.css
 import "./index.css";
-//Import App
-import App from "./App";
+import App from "./components/App";
 
-//Start ROOT
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//renderizamos la vista del App y asi para las pestañas que sea
 root.render(
   <React.StrictMode>
     <App />
